040545356dd1e19383c5b18490ee70b405b0e7ef
EvertonLDS815 <evertonlino1234@gmail.com>
2023-09-03 20:04:31 -0300

    feat: add Simple Conditional

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double saldo = 25.0;
+        double valorSolicitado = 26.0;
+
+        if (valorSolicitado < saldo)
+            saldo = saldo - valorSolicitado;
+
+        System.out.println(saldo);
+    }
+}
