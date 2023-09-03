1246528ae5ad98d0bd0bf0adcb859cc8a4069f16
EvertonLDS815 <evertonlino1234@gmail.com>
2023-09-03 20:40:34 -0300

    feat: add conditional encadeada (chained)

--- src/App.java
+++ src/App.java
@@ -4,6 +4,8 @@ public class App {
 
         if (nota >= 7)
             System.out.println("Aprovado!");
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Prova de recuperação!");
         else
             System.out.println("Reprovado!");
     }
