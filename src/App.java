6c71c2a39fb3e20652ba935808f79581f374f5f2
EvertonLDS815 <evertonlino1234@gmail.com>
2023-09-04 09:03:45 -0300

    feat: try/catch exceptions with java

--- src/App.java
+++ src/App.java
@@ -1,9 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        int nota = 9;
+        try {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        String resultado = nota >= 7 ? "Aprovado" : nota < 7 && nota > 5 ? "Recuperação" : "Reprovado";
+            System.out.println("Digite sua altura: ");
+            double altura = scanner.nextDouble();
 
-        System.out.println(resultado);
+            System.out.println("Digite seu nome: ");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome: ");
+            String sobreNome = scanner.next();
+
+            System.out.println("Digite sua idade: ");
+            int idade = scanner.nextInt();
+
+            System.out.println("Olá, me chamo " + nome + " " + sobreNome);
+            System.out.println("tenho " + idade + " anos");
+            System.out.println("eu tenho " + altura + "cm");
+        } catch (InputMismatchException e) {
+            System.out.println("Os campos idade e/ou altura precisam ser números com virgula!");
+        }
     }
 }
