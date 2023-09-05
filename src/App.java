1bced9fba8a1ce8d595fd4e5924aab7ff35a2fd4
EvertonLDS815 <evertonlino1234@gmail.com>
2023-09-04 21:06:32 -0300

    feat: hierarquia with exceptions

--- src/App.java
+++ src/App.java
@@ -1,29 +1,14 @@
-import java.util.Locale;
-import java.util.Scanner;
-import java.util.InputMismatchException;
+import java.text.NumberFormat;
+import java.text.ParseException;
 
 public class App {
     public static void main(String[] args) throws Exception {
         try {
-            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+            Number valor = NumberFormat.getInstance().parse("a1.75");
 
-            System.out.println("Digite sua altura: ");
-            double altura = scanner.nextDouble();
-
-            System.out.println("Digite seu nome: ");
-            String nome = scanner.next();
-
-            System.out.println("Digite seu sobrenome: ");
-            String sobreNome = scanner.next();
-
-            System.out.println("Digite sua idade: ");
-            int idade = scanner.nextInt();
-
-            System.out.println("Olá, me chamo " + nome + " " + sobreNome);
-            System.out.println("tenho " + idade + " anos");
-            System.out.println("eu tenho " + altura + "cm");
-        } catch (InputMismatchException e) {
-            System.out.println("Os campos idade e/ou altura precisam ser números com virgula!");
+            System.out.println(valor);
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ src/FormatadorCep.java
@@ -0,0 +1,20 @@
+public class FormatadorCep {
+    public static void main(String[] args) {
+
+        try {
+            String cepFormatado = formatarCep("23765064");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidException e) {
+            System.out.println("O cep não corresponde com as regras de negócio");
+        }
+    }
+
+    static String formatarCep(String cep) throws CepInvalidException {
+        if (cep.length() != 8) {
+            throw new CepInvalidException();
+        }
+
+        // simulando um cep formatado
+        return cep;
+    }
+}
